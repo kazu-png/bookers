b1d35ca188712d5ca3fb3fb66512af3e4c4cd9d5
kazu-png <kazuki.t.1006@gmail.com>
2021-04-29 17:57:42 +0000

    [Update]レイアウト直前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+
   end
 
   def show
@@ -18,18 +19,35 @@ class BooksController < ApplicationController
 
   def create
     @books = Book.all
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+   if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book)
+   else
+    render "index"
+    
+   end
   end
+  
   def edit
     @book = Book.find(params[:id])
+
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+      flash[:notice] = "Book was successfully created"
+    redirect_to book_path(@book)
+   else
+    render "edit"
+  end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
